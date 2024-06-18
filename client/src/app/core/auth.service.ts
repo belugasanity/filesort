e37f32cbd88ddebd6d2991eb12bb07985c0cf2a6
Austin <aust.bagley@gmail.com>
2024-06-17 21:37:36 -0600

    logout and is logged in methods

--- client/src/app/core/auth.service.ts
+++ client/src/app/core/auth.service.ts
@@ -17,4 +17,12 @@ export class AuthService {
       })
     )
   }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
 }
