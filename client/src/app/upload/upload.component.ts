d5c4dccb55bde9b70b48de2547872de30e7a709e
Austin <aust.bagley@gmail.com>
2024-06-17 22:14:47 -0600

    add file upload logic

--- client/src/app/upload/upload.component.ts
+++ client/src/app/upload/upload.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FileService } from '../core/file.service';
 
 @Component({
   selector: 'app-upload',
@@ -8,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './upload.component.scss'
 })
 export class UploadComponent {
+  selectedFile: File | null = null;
 
+  constructor(private fileService: FileService) { }
+
+  onFileSelected(event: any) {
+    this.selectedFile = event.target.files[0];
+  }
+
+  onUpload() {
+    if (!this.selectedFile) {
+      alert('Please select a file first.');
+      return;
+    }
+
+    this.fileService.uploadFile(this.selectedFile).subscribe(
+      () => {
+        alert('File uploaded successfully!');
+      },
+      error => {
+        console.error('Upload failed', error);
+        alert('Upload failed. Please try again.');
+      }
+    );
+  }
 }
