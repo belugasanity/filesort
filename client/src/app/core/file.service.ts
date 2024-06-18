2dca9c68cd8fdf8336e17fcd3a0f96e894a327cc
Austin <aust.bagley@gmail.com>
2024-06-17 21:51:35 -0600

    add get fiels method

--- client/src/app/core/file.service.ts
+++ client/src/app/core/file.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,6 +20,15 @@ export class FileService {
     });
 
     return this.http.post(`${this.apiUrl}/upload`, formData, { headers });
+  }
+
+  getFiles(): Observable<any[]> {
+    const token = localStorage.getItem('token');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.get<any[]>(`${this.apiUrl}/files`, { headers });
   }
 
 }
