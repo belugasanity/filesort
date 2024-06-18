f4e5bf9b89a3d369d30d4bf9e5c9332ca0beee93
Austin <aust.bagley@gmail.com>
2024-06-17 21:35:12 -0600

    add service and login method

---
+++ client/src/app/core/auth.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) { }
+
+  login(email: string, password: string) {
+    return this.http.post<any>(`${this.apiUrl}/login`, {email, password}).pipe(
+      map((response: any) => {
+        localStorage.setItem('token', response.data.token);
+      })
+    )
+  }
+}
